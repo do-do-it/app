06cc2a4a12a362f39b367990245ffca988b3ddb1
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-12-04 09:50:20 +0800

    找出args中两数之和为target的下标

---
+++ LeetCode/index.js
@@ -0,0 +1,34 @@
+/**
+ * 找出args中两数之和为target的下标
+ * @param {数组} args 
+ * @param {目标值} target 
+ */
+function findIndexOfArray(args, target) {
+  const sourceArgs = args.slice(0)
+  args.sort((a, b) => {
+    return a - b
+  })
+  console.log(args)
+  let a, b
+  for (let i = 0; i < args.length; i++) {
+    const min = args[i];
+    if (a && b) {
+      break
+    }
+    for (let j = args.length - 1; j >= 0; j--) {
+      const max = args[j];
+      if (j > i) {
+        if (min + max === target) {
+          a = sourceArgs.indexOf(min)
+          b = sourceArgs.indexOf(max)
+          break
+        }
+      } else {
+        break
+      }
+    }
+  }
+  console.log(a, b)
+}
+
+findIndexOfArray([3, 1, 6, 2, 8], 12)
