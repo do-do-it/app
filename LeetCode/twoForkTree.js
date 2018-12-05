47b264cfde01c483963371b0bf1d3ae061772a3f
Min Zhang <zhangmin@MindeMacBook-Pro.local>
2018-12-05 15:46:48 +0800

    1

--- LeetCode/twoForkTree.js
+++ LeetCode/twoForkTree.js
@@ -5,20 +5,19 @@ function twoForkTree(args) {
     this.right = null
   }
   var root = null
-  var insertNode = function(node, key) {
-    if (node === null) {
-      node = new Node(key)
-    } else if (node.key > key) {
-      insertNode(node.left, key)
+  var insertNode = function(node, newNode) {
+    if (node.key > newNode.key) {
+      insertNode(node.left, newNode)
     } else {
-      insertNode(node.right, key)
+      insertNode(node.right, newNode)
     }
   }
   args.forEach(key => {
+    const newNode = new Node(key)
     if (root === null) {
-      root = new Node(key)
+      root = newNode
     } else {
-      insertNode(root, key)
+      insertNode(root, newNode)
     }
   });
   return root
